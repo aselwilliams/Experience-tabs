7b0b044ab9692a8a2bccb34811c0d545463378ea
aselwilliams <aselwilliams22@gmail.com>
2022-05-23 12:21:29 -0400

    fetched the data, added styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {useState, useEffect} from 'react';
 import {FaAngleDoubleRight} from 'react-icons/fa';
 import {ImSpinner9} from 'react-icons/im'
 import './App.css';
-const url='https://course-api.netlify.app/api/react-tabs-project';
+const url='https://course-api.com/react-tabs-project';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -20,13 +20,35 @@ function App() {
 fetchJobs();
   },[])
   
+ if (loading){
+   return (<section className='section loading'><ImSpinner9 /></section>)
+ } 
+  const {company,dates, duties, title} = jobs[value];
   return (
-   <> {loading && <section className='section loading'><ImSpinner9 /></section>}
-    <section className="title">
-    
+    <section className="section">
+      <div className="title">
+    <h2>experience</h2>
+    <div className="underline"></div>
+    </div>
+    <div className="jobs-center">
+      {/* btn-container */}
+      {/* job-info */}
+      <article className='job-info'>
+    <h3>{title}</h3>
+    <h4>{company}</h4>
+    <p className='job-date'>{dates}</p>
+    {duties.map((duty, index)=>{
+      return (
+        <div key={index} className='job-desc'>
+          <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+          <p>{duty}</p>
+        </div>
+      )
+    })}
+      </article>
+    </div>
     </section>
     
-    </>
   );
 }
 
